eba89cdc9395a3772645358ddbc8fc86814b33e0
Felipe Manenti Reis <manentireis16@gmail.com>
2026-09-09 11:05:41 -0300

    ok

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener(¨DOMContentLoaded¨, ()=>(
+    const likeBtn = document.querySelector(¨.Like-btn¨);
+    const postMedia = document.querySelector(¨.post-media¨);
+    if (!likeBtn) return;
+
+    const likesCountSpan = likeBtn.querySelector(¨.likes-count¨);
+    const bookmarkBtn = document.querySelector(¨.bookmark-btn¨);
+
+    let isLiked = false;
+    let baseLikes = 0;
+    
+
+))
